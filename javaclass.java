c88dc22051406b3cd59d91484f30d5e2697af4bd
firuzeOz <66030209+Firuzeoz@users.noreply.github.com>
2022-10-04 02:29:25 -0400

    Update javaclass.java

--- javaclass.java
+++ javaclass.java
@@ -2,5 +2,6 @@ public class javaclass {
     public static void main(String[] args) {
         System.out.println("heyylo!");
         System.out.println("heyyloOOOoo!");
+        System.out.println("heyylo Beybisi!");
     }
 }
